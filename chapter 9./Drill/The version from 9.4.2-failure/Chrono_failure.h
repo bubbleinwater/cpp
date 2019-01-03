02d9a7c2ab149e3bf5cc7e0e2a7199ad5a8a6317
水の中の泡 <hiero.tone.88@gmail.com>
2019-01-03 21:45:29 +0900

    Create Chrono_failure.h

---
+++ chapter 9./Drill/The version from 9.4.2-failure/Chrono_failure.h
@@ -0,0 +1,28 @@
+//---------------------------------
+//2019.jun.3
+//
+//I'm at the Drill of chapter 9
+//C++ had been changed?
+//I tried to use constructor within the scope of the struct Date, as I had written below,
+//but link time error (?I think.) occured.LNK2019 and LNK1120. I'm not sure that the
+//construtor made this linktime error.
+//
+//And I thought I had to write Date(); for Date tomorrow as default constructor, but 
+//I don't have to write costructor here too.(I'm not sure too.)
+//
+//---------------------------------
+
+
+#include"std_lib_facilities.h"
+
+struct Date {
+	int y, m, d;
+//---------------------------------
+//	Date(int y, int m, int d);      //error: LNK2019 and LNK 1120
+//	Date();                         //but I don't know that the code written here really caused these errors or not.
+//--------------------------------- 
+};
+
+Date& add_day(Date& dd, int n);
+
+ostream& operator<<(ostream& os, const Date& d);
