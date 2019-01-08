d2fd54c0635acf7648545eb8b093fa9103f2070f
水の中の泡 <hiero.tone.88@gmail.com>
2019-01-08 23:01:11 +0900

    Update Chrono.cpp

--- chapter 9./Drill/The version from 9.4.3/Chrono.cpp
+++ chapter 9./Drill/The version from 9.4.3/Chrono.cpp
@@ -3,7 +3,7 @@ void Date::add_day(int n) {
 	if (31 < (d + n)) 
 	{
 		d += (n - 31); 
-		(m == 12) ? m = 1, ++y : ++m;
+		(m == 12) ? m = 1, ++y : ++m;		//I don't know how, but should I write "m = ( m == 12 ) ? ..."  here?
 	}
 	else d += n;
 }
