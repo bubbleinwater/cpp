faa7bc7f3ebcc8fe20ff9852b0e743793ab8e492
水の中の泡 <hiero.tone.88@gmail.com>
2019-01-26 09:22:53 +0900

    Update name_age_main.cpp

--- chapter 9./exercise 1./name_age_main.cpp
+++ chapter 9./exercise 1./name_age_main.cpp
@@ -1,13 +1,29 @@
-//---------------------------------------------------------------
-//2019.jan.24
-//
-//when I had wrote
-//#include"name_age.cpp"
-//error : LNK2005 and LNK1169
-//occured
-//at the drill chapter 9, I wrote like this but this kind of error did not occured.
-//I cannot understand!!!!!
-//---------------------------------------------------------------
+/*---------------------------------------------------------------
+2019.jan.24
+dear me from the future. or someone else
+when I had wrote
+#include"name_age.cpp"
+error : LNK2005 and LNK1169
+occured
+at the drill chapter 9, I wrote like this but this kind of error did not occured.
+I cannot understand!!!!!
+
+
+
+commenting starts:
+simple namne pairs holder
+
+this program hold name and age pairs.
+
+	Statement:
+class Name_pairs
+read_names()//this function read cin and push it back to vector<string> name. once user finish entering name, this function calls read_age() function for each names
+read_age();
+sort()
+print()//this function prints names and ages out from vector<string> name and vector<double> age separately.
+
+--------------------------------------------------------------
+*/
 
 #include"name_age.h"
 
