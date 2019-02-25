2174ac921b8d0671775847960584818639bd4be1
水の中の泡 <hiero.tone.88@gmail.com>
2019-02-25 22:49:38 +0900

    Update isbn_code.h

--- chapter 9./exercise 5./isbn_code.h
+++ chapter 9./exercise 5./isbn_code.h
@@ -16,15 +16,17 @@ namespace Book {
 	class Book {
 	public:
 		class invalid_data {};//for throwing exception
-		string ISBN()const { return isbn; }
+		class invalid_isbn {};//invalid ISBN
+		string ISBN()const { return isbn; }				
+		void checking_ISBN(string isbn);
 		string TITLE()const { return title; }
 		string AUTHOR() const { return author; }
 		string COPYRIGHT_DATE()const { return copyright_Date; }
 		bool IS_CHECKEDOUT()const { return is_checkedout; }//when you call this, this function return you 
 		void storing_book();//add new book's data
 		vector<Book> vB;
-		bool checkout(Book checked_out);//to checkout 
-		bool checkin(Book checked_in);//to checkin
+		void checkout(Book& checked_out);//to checkout 
+		void checkin(Book& checked_in);//to checkin
 
 		Book();
 		Book(string i, string t, string a, string c, bool is_c);
