b9504e5b938d9ce9b933b1ce7530599c88d6cf8b
水の中の泡 <hiero.tone.88@gmail.com>
2019-03-25 21:30:21 +0900

    Update isbn_code.h

--- chapter 9./exercise 7./isbn_code.h
+++ chapter 9./exercise 7./isbn_code.h
@@ -15,13 +15,12 @@ namespace Book {
 		string ISBN()const { return isbn; }
 		void checking_ISBN(string isbn);
 		void checking_date(string d);
+		void checking_genre(string isbn, Genre g);
 		string TITLE()const { return title; }
 		string AUTHOR() const { return author; }
 		Genre GENRE() const { return genre; }
 		string COPYRIGHT_DATE()const { return copyright_Date; }
 		bool IS_CHECKEDOUT()const { return is_checkedout; }//when you call this, this function return you 
-		void storing_book();//add new book's data
-		vector<Book> vB;
 		void checkout(Book& checked_out);//to checkout 
 		void checkin(Book& checked_in);//to checkin
 
@@ -38,11 +37,36 @@ namespace Book {
 
 	};
 
-
 	bool operator==(const Book& a, const Book& b);
-
 	bool operator!=(const Book& a, const Book& b);
 
 	ostream& operator<<(ostream& os, const Book& a);
 
+/*
+	class patron {
+	public:
+		string USER_NAME() { return user_name; }
+		string CARD_NUMBER() { return card_number; }
+		string FEE() { return fee; }
+		void set_fee(string f) {
+			if (!(owe_fee)) { 
+				fee = f; 
+				owe_fee = true;
+			}
+			else { 
+				cout << "changed fee from " << fee << " to "<< f <<".\n";
+				fee = f;
+			}
+		}
+
+
+	private:
+		string user_name;
+		string card_number;
+		bool owe_fee;
+		string fee;
+
+	};
+	*/
+	Book& storing_book(Book& b);//add new book's data
 }
