298c17e279253f04a6e1090fceef3666f0c6f1bf
水の中の泡 <hiero.tone.88@gmail.com>
2019-03-25 22:03:56 +0900

    Update isbn_code_main.cpp

--- chapter 9./exercise 8./isbn_code_main.cpp
+++ chapter 9./exercise 8./isbn_code_main.cpp
@@ -1,14 +1,11 @@
 #include "isbn_code.h"
 
-int main() 
+int main()
 try {
 
 	Book::Book bookisbn;
-	bookisbn.storing_book();
-	Book::Book bk = bookisbn;
-
-
-	cout << bookisbn;
+	
+	cout << storing_book(bookisbn);
 
 	keep_window_open();
 	return 0;
@@ -16,8 +13,10 @@ try {
 }catch(Book::Book::invalid_date){
 	cerr << "error::invalid date\n";
 	keep_window_open();
-}
-catch (Book::Book::invalid_isbn) {
+}catch (Book::Book::invalid_isbn) {
 	cerr << "error::invalid ISBN\n";
+	keep_window_open();
+}catch (Book::Book::invalid_data) {
+	cerr << "error::invalid data\n";
 	keep_window_open();
 }
