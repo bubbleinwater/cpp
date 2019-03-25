6a7df4dd949df883e638f13bf38547871f365ab8
水の中の泡 <hiero.tone.88@gmail.com>
2019-03-25 14:19:23 +0900

    Update isbn_code.h

--- chapter 9./exercise 6./isbn_code.h
+++ chapter 9./exercise 6./isbn_code.h
@@ -1,19 +1,11 @@
-/*
-2019.feb.21
-dealing with ISBN-code for library
-2019.feb.23
-add comment
-add check in and out code
-2019.mar.10
-add "==" "!=" "<<"
-*/
-
-
-
 
 #include"std_lib_facilities.h"
 
 namespace Book {
+
+	enum class Genre {
+		fiction = 1, nonfiction, periodical, biography, children, others
+	};
 
 	class Book {
 	public:
@@ -26,28 +18,56 @@ namespace Book {
 		void checking_date(string d);
 		string TITLE()const { return title; }
 		string AUTHOR() const { return author; }
+		Genre GENRE() const { return genre; }
 		string COPYRIGHT_DATE()const { return copyright_Date; }
 		bool IS_CHECKEDOUT()const { return is_checkedout; }//when you call this, this function return you 
-		void storing_book();//add new book's data
-		vector<Book> vB;
 		void checkout(Book& checked_out);//to checkout 
 		void checkin(Book& checked_in);//to checkin
 
 		Book();
-		Book(string i, string t, string a, string c, bool is_c);
+		Book(string i, string t, string a,Genre g, string c, bool is_c);
 
 	private:
 		string isbn;
 		string title;
 		string author;
+		Genre genre;
 		string copyright_Date;
 		bool is_checkedout;
 
 	};
 
 	bool operator==(const Book& a, const Book& b);
-
 	bool operator!=(const Book& a, const Book& b);
 
 	ostream& operator<<(ostream& os, const Book& a);
+//	ostream& operator<<(ostream& os, Genre& g);
+
+/*
+	class patron {
+	public:
+		string USER_NAME() { return user_name; }
+		string CARD_NUMBER() { return card_number; }
+		string FEE() { return fee; }
+		void set_fee(string f) {
+			if (!(owe_fee)) { 
+				fee = f; 
+				owe_fee = true;
+			}
+			else { 
+				cout << "changed fee from " << fee << " to "<< f <<".\n";
+				fee = f;
+			}
+		}
+
+
+	private:
+		string user_name;
+		string card_number;
+		bool owe_fee;
+		string fee;
+
+	};
+	*/
+	Book& storing_book(Book& b);//add new book's data
 }
