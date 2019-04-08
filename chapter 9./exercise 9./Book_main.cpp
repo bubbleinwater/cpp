f8d21ae2d4914f36a12a92e1abed480560b26516
水の中の泡 <hiero.tone.88@gmail.com>
2019-04-08 20:14:31 +0900

    Update Book_main.cpp

--- chapter 9./exercise 9./Book_main.cpp
+++ chapter 9./exercise 9./Book_main.cpp
@@ -1,30 +1,46 @@
 #include "isbn_code.h"
 
-
 int main()
 try {
-	
-	Book::Book bookisbn;
-	cout << storing_book(bookisbn);
 
-	Book::patron pt;	
-	cout << set_patron(pt);
+	Book::Library library_for_vector;
 
-
-
-	keep_window_open();
+	string st = "";
+	while (st != "exit") {
+		cout << "Please enter, addbook or addpatron or displaybooks or displaypatrons or exit.\n";
+		cin >> st;
+		if (st == "addbook") {
+			Book::Book bookisbn;
+			library_for_vector.add_book(bookisbn);
+		}
+		else if (st == "addpatron") {
+			Book::Patron pt;
+			library_for_vector.add_patron(pt);
+		}
+		else if (st == "displaybooks") {
+			library_for_vector.display_books();
+		}
+		else if (st == "displaypatrons") {
+			library_for_vector.display_patrons();
+		}
+		else {}
+	}
+//	keep_window_open();
 	return 0;
-
-}catch(Book::Book::invalid_date){
+}
+catch(Book::Book::invalid_date){
 	cerr << "error:invalid date\n";
 	keep_window_open();
-}catch (Book::Book::invalid_isbn) {
+}
+catch (Book::Book::invalid_isbn) {
 	cerr << "error:invalid ISBN\n";
 	keep_window_open();
-}catch (Book::Book::invalid_data) {
+}
+catch (Book::Book::invalid_data) {
 	cerr << "error:invalid data\n";
 	keep_window_open();
-}catch (Book::Book::invalid_genre) {
+}
+catch (Book::Book::invalid_genre) {
 	cerr << "error:invalid Genre\n";
 	keep_window_open();
 }
