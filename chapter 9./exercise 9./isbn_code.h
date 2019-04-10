c4ee580a9a460f4fd63190d89f269cbc2327a57a
水の中の泡 <hiero.tone.88@gmail.com>
2019-04-10 23:38:05 +0900

    Update isbn_code.h

--- chapter 9./exercise 9./isbn_code.h
+++ chapter 9./exercise 9./isbn_code.h
@@ -1,18 +1,45 @@
 #include"std_lib_facilities.h"
 #include"Chrono.h"
 
+
 namespace Book {
 
 	enum class Genre {
 		fiction = 1, nonfiction, periodical, biography, children, others
 	};
 
+	class Patron {
+	public:
+		bool OWE_FEE() const { return owe_fee; }
+		string USER_NAME() const { return user_name; }
+		string CARD_NUMBER() const { return card_number; }
+		string FEE() const { return fee; }
+		bool IN_THE_LIBRARY()const { if (in_the_library)return true; else return false; }
+		bool free(Patron p) { if (fee == "0")return true; }
+		void set_fee(string f);
+
+		Patron();
+		Patron(bool o_f, string u_n, string c_n, string f, bool in_lib);
+
+	private:
+		bool owe_fee;
+		string user_name;
+		string card_number;
+		string fee;
+		bool in_the_library;
+
+	};
+
 	class Book {
 	public:
+
 		class invalid_date {};//for throwing exception
 		class invalid_isbn {};//invalid ISBN
 		class invalid_data {};
 		class invalid_genre {};//for invalid genre
+
+		void checkout() { is_checkedout = true; }//to checkout 
+		void checkin() { is_checkedout = false; }//to checkin
 
 		string ISBN()const { return isbn; }
 		void checking_ISBN(string isbn);
@@ -23,10 +50,9 @@ namespace Book {
 		Genre GENRE() const { return genre; }
 		string COPYRIGHT_DATE()const { return copyright_Date; }
 		Chrono::Date Chrono_COPYRIGHT_DATE()const{ return chrono_copyright_Date; }
+		string LOANABLE()const{ if (is_checkedout) { return "NO"; } else { return "YES"; } }
 		bool IS_CHECKEDOUT()const { return is_checkedout; }//when you call this, this function return you 
-		void checkout();//to checkout 
-		void checkin();//to checkin
-
+		
 		Book();
 		Book(string i, string t, string a,Genre g, Chrono::Date c, bool is_c);
 
@@ -45,28 +71,6 @@ namespace Book {
 	bool operator!=(const Book& a, const Book& b);
 
 	ostream& operator<<(ostream& os, const Book& a);
-
-	class Patron {
-	public:
-		bool OWE_FEE() const{ return owe_fee; }
-		string USER_NAME() const { return user_name; }
-		string CARD_NUMBER() const { return card_number; }
-		string FEE() const { return fee; }
-		bool IN_THE_LIBRARY()const{ return in_the_library; }
-		bool free(Patron p) { if (fee == "0")return true; }
-		void set_fee(string f);
-
-		Patron();
-		Patron(bool o_f, string u_n, string c_n, string f, bool in_lib);
-
-	private:
-		bool owe_fee;
-		string user_name;
-		string card_number;
-		string fee;
-		bool in_the_library;
-
-	};
 
 	ostream& operator<<(ostream& os, const Patron& pt);
 
@@ -88,7 +92,8 @@ namespace Book {
 		void add_patron(Patron pt);
 		void display_patrons();
 
-		void checkout_book(Book& b,Patron p);
+		void checkout_book(Book& b, Patron& p,vector<Book>& v_b);
+		void checkin_book(Book& b, Patron& p, vector<Book>& v_b);
 
 		Library();
 		Library(Book b,Patron p,Transaction t);
