c8dae6e19f3f5b5263eb6cdf99ef583f0a9a2832
水の中の泡 <hiero.tone.88@gmail.com>
2019-04-23 22:08:18 +0900

    Update Chrono.cpp

--- chapter 9./exercise 10./Chrono.cpp
+++ chapter 9./exercise 10./Chrono.cpp
@@ -16,16 +16,33 @@ namespace Chrono {
 	Date::Date(int yy, Month mm, int dd)
 		: y{ yy }, m{ mm }, d{ dd }//initialize data members
 	{
-		if (!is_valid())throw Invalid{};//check for validity
+		if (!is_valid())
+			throw Invalid();//check for validity
 	}
 
 	bool Date::is_valid()//return true if date is valid
 	{
-		if (int(m) < 0 || 11 < int(m))return false;
-		if (d < 1 || 31 < d)return false;
 		if (y < 0)return false;
-
-		return true;
+//----------
+		if (m == Month::jan || m == Month::mar || m == Month::may || m == Month::jul || m == Month::aug || m == Month::oct || m == Month::dec) {
+			if (d < 1 ||31 < d) { return false; }
+			else return true;
+		}
+		else if (m == Month::apr || m == Month::jun || m == Month::sep || m == Month::nov) {
+			if (d < 1 ||30 < d) { return false; }
+			else return false;
+		}
+		else if( m == Month::feb){
+			if (leapyear(y)) {
+				if (d < 1 ||29 < d) { return false; }
+				else return true;
+			}
+			else {
+				if (d < 1 ||28 < d) { return false; }
+				else return true;
+			}
+		}
+		else return false;		
 	}
 
 	int Date::month() const { //month() returns int not Month is error-prone
@@ -40,14 +57,46 @@ namespace Chrono {
 
 	void Date::add_day(int n) {
 
-		if (31 < (d + n))
-		{
-			d += (n - 31);
-			if (m == Month::dec) { m = Month::jan, ++y; }
-			else { ++m; }
+		if (m == Month::jan || m == Month::mar || m == Month::may || m == Month::jul || m == Month::aug || m == Month::oct || m == Month::dec) {
+//handle month(1,3,5,7,8,10,12)
+			if (31 < (d + n))//when the size of day is larger than 31,m will be assigned by next month 
+			{
+				d += (n - 31);
+				if (m == Month::dec) { m = Month::jan, ++y; }
+				else { ++m; }
+			}
+			else d += n;
 		}
-		else d += n;
+
+		else if (m == Month::apr || m == Month::jun || m == Month::sep || m == Month::nov) {
+			//handle month(4,6,9,11)
+			if (30 < (d + n))//when the size of day is larger than 31,m will be assigned by next month 
+			{
+				d += (n - 30);
+				++m;
+			}
+			else d += n;
+		}
+		else {
+			if (leapyear(y)) {
+				if (29 < (d + n))
+				{
+					d += (n - 29);
+					++m;
+				}
+				else d += n;
+			}
+			else {
+				if (28 < (d + n))
+				{
+					d += (n - 28);
+					++m;
+				}
+				else d += n;
+			}
+		}
 	}
+
 
 	void Date::add_year(int n) {
 		if(m == Month::feb && d == 29 && !leapyear(y + n)){
@@ -57,6 +106,17 @@ namespace Chrono {
 		y += n;
 
 	}
+/*
+	Date next_workday(Date d) {
+//next_workday(today) returns wednesday's date if it is tuesday today.
+//note also this function's range starts from 2000.01.01(is saturday)
+		if (d.year() >= 2000) {
+			for (int i = 0; i < d.month(); ++i) {
+
+			}
+		}
+	}
+	*/
 
 	bool Chrono::leapyear(int y) {
 		int leap = 0;
