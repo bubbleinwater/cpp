73f58c13d8080209187f368110631c6f361c76cd
bubbleinwater <hiero.tone.88@gmail.com>
2019-05-19 01:17:06 +0900

    ex13少し進歩

--- chapter 9/exercise 13/main.cpp
+++ chapter 9/exercise 13/main.cpp
@@ -2,19 +2,22 @@
 
 int main()
 {
-
 	Rational::Rational ra1, ra2;
 
 	cin >> ra1;
 
-	cout << ra1;
+	cout << ra1.reduction();
 
 	ra2 = { 5,'/', 7 };
-	cout << ra2;
+	cout << ra2.reduction();
+
+	cout << (ra1 + ra2).reduction();
+
+	cout << (ra1 - ra2).reduction();
 
 	ra1 = ra2;
 
-	cout << ra1;
+	cout << ra1.reduction();
 
 	keep_window_open();
 	return 0;
