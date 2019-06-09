f117ecf77eacf6dd5f844873b8584775ff5d11d2
bubbleinwater <hiero.tone.88@gmail.com>
2019-06-10 01:55:51 +0900

    I'm beaten

--- chapter 9/exercise 14/Money.cpp
+++ chapter 9/exercise 14/Money.cpp
@@ -5,8 +5,9 @@ namespace Money {
 		:amount_of_money{0}
 	{}
 
-	Money::Money(long int m)
+	Money::Money(string c,long int m)
 	{
+		currency = c;
 		double mm = m;
 		long int M = long int(mm);
 		if ((mm - M) == 0) { amount_of_money = { M }; }
@@ -33,93 +34,126 @@ namespace Money {
 
 //helper function
 
-	ostream& operator<<(ostream& os, Money& m) {
-		if (m.CENT() < 10) {
-			return os << "$" << m.DOLLAR() << ".0" << m.CENT() << "\n";
+	Money kroner_to_dollar(Money kroner) {
+		//	Money result = { "USD",("USD",kroner) * 6.95 };
+		if (kroner.CURRENCY() == "USD") { return kroner; }
+		else {
+			double num = kroner.AOM() * 6.95;
+			int i = 0;
+			for (; i < num; ++i) {}
+			num -= i;
+			if (num < 0.5) {//rounds down
+				Money result = { "USD",i };
+				return result;
+			}
+			else {//or rounds up
+				Money result = { "USD",(i + 1) };
+				return result;
+			}
 		}
-		else { return os << "$" << m.DOLLAR() << "." << m.CENT() << "\n"; }
+	}
+	ostream& operator<<(ostream& os, Money& m) {
+		Money tmp = kroner_to_dollar(m);
+//		if (tmp.CURRENCY() == "KKD") { m = kroner_to_dollar(tmp); }
+
+		if (tmp.CURRENCY() == "USD") {
+			if (tmp.CENT() < 10) {
+				return os << "USD" << tmp.DOLLAR() << ".0" << tmp.CENT() << "\n";
+			}
+			else { return os << "USD" << tmp.DOLLAR() << "." << tmp.CENT() << "\n"; }
+		}
+		else { throw Money::Invalid(); }
 	}
 
 
 	istream& operator>>(istream& is, Money& m) {
 		long int dollar, cent, result;
-		string st1 = " ";
-		char ch2 = ' ';
-
-		is >> st1;
-//		if (st1 != "$" || st1 != "USD" || st1!= "DKK") { throw Money::Invalid(); }
+		char ch1 = ' ';
+		string st1;
 
 //123.05 should be 12305 .and 123.4 should be 1234 not 12304
 //,but I could not figure it out. so user has to enter $123.40 for $123.4 
-
-
-		is >> dollar;
-		is >> ch2;
-
-		if (ch2 == '.') {
-			is >> cent;
-			if (100 < cent && cent < 1000) {
-				long int tmp = cent;
-				tmp /= 10;
-				tmp *= 10;
-				tmp -= cent;
-				cent /= 10;
-				if (tmp <= -5) { cent += 1; }//tmp will be negative number
-				else {}
-				result = (dollar * 100) + cent;
-				m = { result };
-				return is;
+		while (is >> ch1) {
+			if (ch1 != '.') {
+				switch (ch1) {
+				case'1':case'2': case'3':case'4':case'5':
+				case'6':case'7':case'8':case'9':case'0':
+					cin.putback(ch1);
+					is >> dollar;
+					break;
+				default:
+					st1 += ch1;
+				}
 			}
-			else if (1000 < cent) { throw Money::Invalid(); }
+			else if (ch1 == '.') {
+				is >> cent;
+				if (100 < cent && cent < 1000) {
+					long int tmp = cent;
+					tmp /= 10;
+					tmp *= 10;
+					tmp -= cent;
+					cent /= 10;
+					if (tmp <= -5) { cent += 1; }//tmp will be negative number
+					else {}
+					result = (dollar * 100) + cent;
+					m = { st1,result };
+					return is;
+				}
+				else if (1000 < cent) { throw Money::Invalid(); }
+				else {
+					result = (dollar * 100) + cent;
+					m = { st1,result };
+					return is;
+				}
+			}
 			else {
-				result = (dollar * 100) + cent;
-				m = { result };
-				return is;
+				throw Money::Invalid();
 			}
-/*		else {
-			result = (dollar * 100) + cent;
-			m = { result };
-			return is;
-		}*/
-		}
-/*		else if (ch2 == '$') {
-			cin.putback(ch2);
-			result = dollar * 100;
-			m = { result };
-			return is;
-		}*/
-		else {
-			throw Money::Invalid();
 		}
 	}
 
-	Money& operator+(Money a, Money b) {
+	Money& operator+(Money& a, Money& b) {
+		if (a.CURRENCY() == "KKD") { a = kroner_to_dollar(a); }
+		if (b.CURRENCY() == "KKD") { b = kroner_to_dollar(b); }
+		
 		Money result;
-		result = ((a.DOLLAR() + b.DOLLAR()) * 100 + (a.CENT() + b.CENT()));
+		result = 
+//		{"USD",((a.DOLLAR() + b.DOLLAR()) * 100 + (a.CENT() + b.CENT())) };
+		{"USD", a.AOM() + b.AOM()};
+		cout << "for check: " << result;
 		return result;
 	}
 
 	Money& operator-(Money a, Money b) {
+		Money tmp = a;
+		if (tmp.CURRENCY() == "KKD") { a = kroner_to_dollar(tmp); }
+		tmp = b;
+		if (tmp.CURRENCY() == "KKD") { b = kroner_to_dollar(tmp); }
+
 		Money result;
-		result = ((a.DOLLAR() - b.DOLLAR()) * 100 + (a.CENT() - b.CENT()));
+		result = {"USD",((a.DOLLAR() - b.DOLLAR()) * 100 + (a.CENT() - b.CENT()))};
 		if (result.AOM() < 0) { throw Money::Invalid(); }
 		return result;
 	}
 
-	Money& operator*(Money& a, double d) {
+	Money& operator*(Money a, double d) {
+		Money tmp = a;
+		if (tmp.CURRENCY() == "KKD") {
+			a = kroner_to_dollar(tmp); 
+		}
+
 		double num = a.AOM() * d;
 		int i = 0;
 		for (; i < num; ++i) {
 		}
 		num -= i;
-		if(num < 0.5){
-			Money result = i;
+		if(num < 0.5){//rounds down
+			Money result = { "USD",i };
 			return result;
 		}
-		else { 
-			Money result = i + 1;
+		else {//or rounds up
+			Money result = { "USD",i + 1 };
 			return result;
 		}
-
 	}
 }
