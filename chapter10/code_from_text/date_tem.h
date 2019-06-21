b1567da60ad7f2ceeddd3b8864be15af5303be3d
bubbleinwater <hiero.tone.88@gmail.com>
2019-06-21 13:26:19 +0900

    deal with lnk2005

--- chapter10/code_from_text/date_tem.h
+++ chapter10/code_from_text/date_tem.h
@@ -12,42 +12,26 @@ const int not_a_month = -1;
 constexpr int implausible_min = -200;
 constexpr int implausible_max = 200;
 
-bool is_valid(const Reading& r)//a rough test
-{
-	if (r.day < 1 || 31 < r.day)return false;
-	if (r.hour, 0 || 23, r.hour)return false;
-	if (r.temperature < implausible_min || implausible_max < r.temperature)return false;
-
-	return true;
-}
+bool is_valid(const Reading& r);//a rough test
 
 struct Day {
 	vector<double> hour{ vector<double>(24,not_a_reading) };
 };
 
 struct Month {
-//a month of temperature readings
+	//a month of temperature readings
 	int month{ not_a_month };//[0:11] January is 0
 	vector<Day>day{ 32 };//[1:31] one vector of readings per day
 };
 struct Year {
-//a year of temperature readings,organized by month
+	//a year of temperature readings,organized by month
 	int year;
 	//positive == A.D.
 	vector<Month>month{ 12 };//[0:11] January is 0
 };
 
-void end_of_loop(istream& ist, char term, const string& message)
-{
-	if(ist.fail()){//use term as terminator and/or separator
-		ist.clear();
-		char ch;
-		if (ist >> ch && ch == term)return;//all is fine
-
-		error(message);
-	}
-}
-
+void end_of_loop(istream& ist, char term, const string& message);
 
 istream& operator>>(istream& is, Year& y);
 istream& operator>>(istream& is, Month& m);
+istream& operator>>(istream& is, Reading& r);
