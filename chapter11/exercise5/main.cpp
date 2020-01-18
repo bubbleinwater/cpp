f4b2ac3d5eff0a681afd422049c3aae0f39017ec
水の中の泡 <hiero.tone.88@gmail.com>
2020-01-18 22:34:31 +0900

    Update main.cpp

--- chapter11/exercise5/main.cpp
+++ chapter11/exercise5/main.cpp
@@ -1,35 +1,22 @@
-#include"../../std_lib_facilities.h"
+#include"std_lib_facilities.h"
 
-int main()
-{
-	cout << "Please enter some string type values.\n";
-
-	for (char ch; cin.get(ch);) {
-		if (ch == '|') { break; }
-		if(isprint(ch))cout <<"\'"<<ch<<"\' is printable character.\n";
-		else cout << " can not print this character\n";
-
-		if (isspace(ch)) { cout << " It's a space\n"; }
-		else if (isxdigit(ch)) {
-			if (isdigit(ch)) { cout << "\'" << ch << "\'" << " is a digit character.\n"; }
-			else if (isalpha(ch)) {
-				if (isupper(ch)) { cout << "\'" << ch << "\'"<< " is a alphabet,upper case alphsbet character.\n"; }
-				else if (islower(ch)) {
-					cout << "\'" << ch << "\'"<< " is a alphabet, lower case alphsbet character.\n"; }
-			}
+int main() {
+	string s;
+	cout << "enter some character.\n enter | for exit.\n";
+	for (;s != "|";cin >> s) {
+		for (char c : s) {
+			if (isspace(c))cout << c << " is whitespace.\n";
+			if (isalpha(c))cout <<c<< " is letter.\n";
+			if (isdigit(c))cout << c << " is decimal digit.\n";
+			if (isxdigit(c))cout << c << " is hexadecimal digit.\n";
+			if (isupper(c))cout << c << " is an uppercase leter.\n";
+			if (islower(c))cout << c << " is a lowercase letter.\n";
+			if (isalnum(c))cout << c << " is a letter or a decimal digit.\n";
+			if (iscntrl(c))cout << c << " is a control character.\n";
+			if (ispunct(c))cout << c << " is not a letter, digit, whitespace, or invisible control character.\n";
+			if (isprint(c))cout << c << " is printable.\n";
+			if (isgraph(c))cout << c << " is not a space.\n";
 		}
-		else if (isalpha(ch)) {
-			if (isupper(ch)) { cout << "\'" << ch << "\'" << " is a alphabet,upper case alphsbet character.\n"; }
-			else if (islower(ch)) {
-				cout << "\'" << ch << "\'" << " is a alphabet, lower case alphsbet character.\n";
-			}
-		}
-		else if (iscntrl(ch)) {
-			cout << "\'" << ch << "\'" << " is a control character\n";
-		}
-		else if (ispunct(ch)) { cout << "\'" << ch << "\'" << " is a punctutation character.\n"; }
-
-		else if (isgraph(ch)) { cout << "\'" << ch << "\'" << " is a not-space character.\n"; }
 	}
 
 	keep_window_open();
