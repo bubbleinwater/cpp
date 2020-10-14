1380149052ca268a3547129f274152b369b565dd
bubbleinwater <hiero.tone.88@gmail.com>
2020-10-14 21:36:23 +0900

    ch13ex2
    now users can use the default corner radius

--- chapter13/exercise2/Box.h
+++ chapter13/exercise2/Box.h
@@ -10,6 +10,12 @@ namespace Graph_lib {
 		}
 		Boxb(Point p1,int w,int h,double rad):topl{p1},wid{w},hei{h}, radius{ rad }{
 		}
+		Boxb(Point p1, Point p2) :topl{ p1 }, wid{ p2.x - p1.x }, hei{ p2.y - p1.y }
+		{
+
+		}
+		Boxb(Point p1, int w, int h) :topl{ p1 }, wid{ w }, hei{ h }{
+		}
 
 		void draw_lines()const;
 
